fece66c8d7b440fd82911445d3c1463d7bd1d8ce
Alina-Sapunova <alina090207@mail.ru>
2024-01-16 22:11:00 +0300

    add class Camera

--- Game.py
+++ Game.py
@@ -185,6 +185,24 @@ def text_name_game(screen):
 #         pygame.display.flip()
 #     pygame.quit()
 
+class Camera:
+    # зададим начальный сдвиг камеры
+    def __init__(self):
+        self.dx = 0
+        self.dy = 0
+
+    # сдвинуть объект obj на смещение камеры
+    def apply(self, obj):
+        obj.rect.x += self.dx
+        obj.rect.y += self.dy
+
+    # позиционировать камеру на объекте target
+    def update(self, target):
+        self.dx = -(target.rect.x + target.rect.w // 2 - WIDTH // 2)
+        self.dy = -(target.rect.y + target.rect.h // 2 - HEIGHT // 2)
+
+
+camera = Camera()
 
 if __name__ == "__main__":
     pygame.init()
