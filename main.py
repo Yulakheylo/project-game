0e5a426b0f8ba108fa3e63a684c27b3aff5af848
Yulakheylo <yulakheylo@gmail.com>
2024-01-20 12:21:16 +0300

    coog

--- main.py
+++ main.py
@@ -589,7 +589,7 @@ def men():
         kartinka = pygame.transform.scale(kartinka, (size))
 
         font_count_coins = pygame.font.Font(None, 42)
-        count_coins_text = font_count_coins.render(f'Лучший результат:30монет', 1, (0, 0, 0))
+        count_coins_text = font_count_coins.render(f'Лучший результат:36монет', 1, (0, 0, 0))
         #knopka_strelka = StartMenu(WIDTH / 2 - (600 / 2), 450, 100, 70, 'images/strelka.png', '')
         running = True
         while running:
